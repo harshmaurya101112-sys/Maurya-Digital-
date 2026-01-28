79eb2cb70d7f1b72881c9fb90b94bee738fedd20
harshmaurya101112-sys <harsh.maurya101112@gmail.com>
2026-01-28 15:35:37 +0530

    Update ServiceCard.tsx

--- ServiceCard.tsx
+++ ServiceCard.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ArrowRight } from 'lucide-react';
+import { ArrowRight→ } from 'lucide-react';
 import { Service } from '../types';
 
 interface ServiceCardProps {
